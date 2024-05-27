f048a7289020d787b3ca40067ca05564441aa8d5
UncleBimBim <kymlamsam@gmail.com>
2024-05-27 12:55:40 +1000

    Update index.ts

--- src/frames/index.ts
+++ src/frames/index.ts
@@ -3,6 +3,7 @@ import count from "./count.js";
 import mint from "./mint.js";
 import transaction from "./transaction.js";
 import stolen from "./stolen.js";
+import "./buck5gm.js";
 
 export default {
     count,
